83645eb9e0dc621e6446fcbe271b254074620f9a
IlyasMagomedaminov <imagomedaminov00@mail.ru>
2023-03-27 22:15:19 +0300

    Циклы: 0.1 сделал

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
         task2();
         task3();
         task4();
-        //task5();
-        //task6();
-        //task7();
-        //task8();
+        task5();
+        task6();
+        task7();
+        task8();
     }
     public static void task1(){
         System.out.println("Задача 1.");
@@ -54,6 +54,74 @@ public class Main {
     public static void task4(){
         System.out.println("Задача 4.");
 
+        int salary = 15_000;
+        byte procent = 7;
+        int finalSumm = 12_000_000;
+        short month = 0;
+
+        while (salary < finalSumm){
+            salary = salary + procent * salary / 100 ;
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + salary + " рублей");
+        }
+    }
+    public static void task5(){
+        System.out.println("задача 5");
+
+        int salary = 15_000;
+        byte procent = 7;
+        int finalSumm = 12_000_000;
+        short month = 0;
+
+        while (salary < finalSumm){
+            salary = salary + procent * salary / 100 ;
+            month++;
+            if(month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + salary + " рублей");
+            }
+        }
+    }
+    public static void task6(){
+        System.out.println("Задача 6");
+
+        int salary = 15_000;
+        byte procent = 7;
+        int years =12 * 9;
+        short month = 0;
+
+
+        while (month < years ){
+            salary = salary + procent * salary / 100 ;
+            month++;
+            if(month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + salary + " рублей");
+            }
+        }
+    }
+    public static void task7(){
+        System.out.println("Задача 7");
+
+        int month = 31;
+        int day = 5;
+
+        while(day < month){
+            if(day >= 5){
+                System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
+            }
+            day+=7;
+        }
+    }
+    public static  void task8(){
+        System.out.println("Задача 8");
+
+        short max = 2023 + 100;
+        short min = 2023 - 200;
+
+        for (int i = 0;i < max;i++){
+            if (i % 79 == 0 && i > min ){
+                System.out.println(i);
+            }
+        }
     }
 
 }
